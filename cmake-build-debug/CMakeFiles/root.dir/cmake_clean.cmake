80eab8df35bd6b7b3f84012309bda9b6bcda05aa
Mohammad Emad Changizi Ashtiani <emad.33mohammad35@gmail.com>
2023-01-30 23:27:12 +0330

    first commit

---
+++ cmake-build-debug/CMakeFiles/root.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/root.dir/main.c.o"
+  "CMakeFiles/root.dir/main.c.o.d"
+  "CMakeFiles/root.dir/test.c.o"
+  "CMakeFiles/root.dir/test.c.o.d"
+  "CMakeFiles/root.dir/test2.c.o"
+  "CMakeFiles/root.dir/test2.c.o.d"
+  "root"
+  "root.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/root.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
